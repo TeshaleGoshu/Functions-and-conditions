140526db4706e1dedcc940f2c49895fd29264589
Teshale Goshu Gobena <teshalegoshu1@gmail.com>
2023-04-23 13:16:34 +0100

    commit

---
+++ JS/script.js
@@ -0,0 +1,81 @@
+// function myFirst(){
+//     console.log("Hello")
+// }
+// function mySecond (x){
+//     console.log("mySecond")
+//     console.log("x")
+// }
+
+// function myThird (){
+//     console.log("myThird")
+// }
+
+// function myFourth () {
+//     console.log Array ([you,our,6,true])
+// }
+
+// function myFifth(){
+//     console.log(50,40)
+//     console.log(50+60)
+
+// }
+//
+
+// myFirst()
+// mySecond()
+// myThird()
+// myFourth(0)
+// myFifth()
+// function JimmaAbbaJifar(x,y){
+// if (x===y)
+// {console.log("correct 👍" );}
+
+//  else{console.log("incorrect 😒");
+
+// JimmaAbbaJifar ("x","y");
+
+// function JimmaAbbaJifar (test1,test2){
+//    (test1===test2)? console.log("correct"):console.log("incorrect");
+//  }
+//  JimmaAbbaJifar ("test1","test1")
+
+function LetsEat() {
+	let day = new Date();
+	console.log(day.getHours());
+
+	let Eat;
+	switch (true) {
+		case day.getHours() > 7 && day.getHours() < 10:
+			Eat = "breakfast";
+			break;
+		case day.getHours() >= 12 && day.getHours() < 18:
+			Eat = "Lunch";
+			break;
+			
+			// case day.getHours()>12&&day.getHours()>18:
+			// Eat = "dinner"
+
+			
+// 		default:
+// 			Eat = "Fasting";
+// 	}
+
+// 	return Eat;
+// }
+
+// console.log(LetsEat());
+
+// console.log (LetsEat ("morning"));
+// console.log(LetsEat("midday"));
+// console.log(LetsEat("evening"));
+// console.log(LetsEat(""));
+// console.log(LetsEat("14:00"));
+// function ToDay (day){
+// let x = new Date();
+
+// }
+// return ToDay;
+// console.log(ToDay (day));
+
+// console.log(typeof "7")
+
